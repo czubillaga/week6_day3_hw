19d62926511e948683c6110d06be9b7fa6582976
Carlos Zubillaga <carloszubillaga@Carloss-MacBook-Pro.local>
2021-10-06 15:15:58 +0100

    panagram done

--- hw_advanced_start/pangram_finder/pangram_finder.js
+++ hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,12 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase.toLowerCase().split('');
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every((character) => {
+    return this.phrase.includes(character);
+  });
 }
 
 module.exports = PangramFinder;
