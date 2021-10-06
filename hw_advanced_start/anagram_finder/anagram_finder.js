d11105cdfdbc0409c4fa45a5611766f48db953ea
Carlos Zubillaga <carloszubillaga@Carloss-MacBook-Pro.local>
2021-10-06 16:17:28 +0100

    final commit

--- hw_advanced_start/anagram_finder/anagram_finder.js
+++ hw_advanced_start/anagram_finder/anagram_finder.js
@@ -1,9 +1,15 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
+    this.chars = word.toLowerCase().split('');
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    return otherWords.filter((currentWord) => {
+        let currentChars = currentWord.toLowerCase().split('');
+        return currentChars.every((char) => {
+           return this.chars.includes(char);
+        }) && currentWord !== this.word && currentWord !== "" && currentChars.length == this.chars.length;
+    });
 }
 
 module.exports = AnagramFinder;
